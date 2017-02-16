aaf3bc51e117313613a034a5c687b26837dc1670
ZeroDude13 <thadpeterson93@gmail.com>
2017-02-15 21:17:26 -0800

    Update cone.js

--- Assignment-3/cone.js
+++ Assignment-3/cone.js
@@ -1,9 +1,10 @@
 var gl = null;
+var cone = null;
 
 function init() {
     var canvas = document.getElementById( "webgl-canvas" );
 
-    gl = WebGLUtils.setupWebGL( cone );
+    gl = WebGLUtils.setupWebGL( canvas );
 
     if ( !gl ) {
         alert("Unable to setup WebGL");
@@ -11,12 +12,15 @@ function init() {
     }
 
     gl.clearColor( 0.419608, 0.137255, 0.556863, 1.0 );
+    
+    cone = New Cone( n );
 
     render();
 }
 
 function render() {
     gl.clear( gl.COLOR_BUFFER_BIT );
+    cone.render();
 }
 
 window.onload = init;
