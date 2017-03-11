dddb890f67e26c02a2446677697e6e5994a72a04
ZeroDude13 <thadpeterson93@gmail.com>
2017-03-10 23:16:59 -0800

    Update Cube.js

--- Common/Cube.js
+++ Common/Cube.js
@@ -61,6 +61,12 @@ function Cube( vertexShaderId, fragmentShaderId ) {
     this.indices = { 
         values : new Uint16Array([
             // Add your list of triangle indices here
+            0, 1, 2,      0, 2, 3,    // Front face
+            4, 5, 6,      4, 6, 7,    // Back face
+            8, 9, 10,     8, 10, 11,  // Top face
+            12, 13, 14,   12, 14, 15, // Bottom face
+            16, 17, 18,   16, 18, 19, // Right face
+            20, 21, 22,   20, 22, 23  // Left face
         ])
     };
     this.indices.count = this.indices.values.length;
